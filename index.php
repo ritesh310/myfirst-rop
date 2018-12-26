4923d291630f2a8aedf2ccea809e629f7f1ad9fa
ritesh310 <46154065+ritesh310@users.noreply.github.com>
2018-12-27 11:44:25 +1300

    Latest

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World-Happy Learning. Welcome Ritesh Joshi</p>'; ?> 
+ <?php echo '<p>Hello World-Happy Learning. Welcome Ritesh Joshi!!!!</p>'; ?> 
  </body>
 </html>
